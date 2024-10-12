930544ea53d6e74be50d4fdff27b8d3f54db1aef
Rajveer Singh <125016453+Rajveer-Singh8124@users.noreply.github.com>
2024-10-12 20:45:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='build', static_url_path='/')
+app = Flask(__name__, static_folder='react-calendar-app/build', static_url_path='/')
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
